da7f7042492803e08018cbee82c6d5974ca5b23a
PAple9 <denisnovikov00@bk.ru>
2025-01-28 13:06:21 +0300

    gettes+setters+fix
    add getters+setters. fix an error with completing tasks in one line and code review

--- src/Main.java
+++ src/Main.java
@@ -6,162 +6,162 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
     static File file = new File("tasks.txt");
     static ArrayList<Task> list = new ArrayList<>();
     static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static void main(String[] args) {
-        try{
-        loadFromFile();
+        try {
+            loadFromFile();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Добро пожаловать в ваш список задач!");
-        boolean flag = true;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Добро пожаловать в ваш список задач!");
+            boolean flag = true;
 
-        while(flag){
-            System.out.println("Выберите действие:\n" +
-                    "add - добавить задачу\n" +
-                    "list - список задач\n" +
-                    "done - отметить задачу как выполненную\n" +
-                    "delete - удалить все выполненные задачи\n" +
-                    "search - найти задачи по описанию\n" +
-                    "exit - выход.");
-            String todo = scanner.nextLine();
-
-            switch (todo){
-                case "exit":
-                    System.out.println("Программа завершила работу, задачи сохранены!");
-                    flag = false;
-                    break;
-                case "add":
-                    System.out.println("Что вам нужно сделать еще?");
-                    addTask(scanner.nextLine());
-                    break;
-                case "list":
-                    listTasks();
-                    break;
-                case "done":
-                    System.out.println("Введите номер задачи которую вы выполнили.");
-                    markTaskAsDone(scanner.nextInt());
-                    scanner.nextLine();
-                    break;
-                case "delete":
-                    deleteCompletedTasks();
-                    break;
-                case "search":
-                    System.out.println("Введите ключевое слово для поиска задачи.");
-                    searchTasks(scanner.nextLine());
-                    break;
-                default:
-                    System.out.println("Неизвестная команда: " + todo);
-                    break;
+            while (flag) {
+                System.out.println("Выберите действие:\n" +
+                        "add - добавить задачу\n" +
+                        "list - список задач\n" +
+                        "done - отметить задачу как выполненную\n" +
+                        "delete - удалить все выполненные задачи\n" +
+                        "search - найти задачи по описанию\n" +
+                        "exit - выход.");
+                String todo = scanner.nextLine();
+                String[] command = todo.split(" ");
+                switch (command[0]) {
+                    case "exit":
+                        System.out.println("Программа завершила работу, задачи сохранены!");
+                        flag = false;
+                        break;
+                    case "add":
+                        if(command.length < 2){
+                            System.out.println("Вводи команду и данные в одну строку!");
+                        }
+                        else{
+                            addTask(todo.substring(4));
+                        }
+                        break;
+                    case "list":
+                        listTasks();
+                        break;
+                    case "done":
+                        if(command.length < 2){
+                            System.out.println("Вводи команду и данные в одну строку!");
+                        }
+                        markTaskAsDone(Integer.parseInt(command[1]));
+                        break;
+                    case "delete":
+                        deleteCompletedTasks();
+                        break;
+                    case "search":
+                        if(command.length < 2){
+                            System.out.println("Вводи команду и данные в одну строку!");
+                        }
+                        searchTasks(todo.substring(7));
+                        break;
+                    default:
+                        System.out.println("Неизвестная команда: " + todo);
+                        break;
                 }
             }
-        saveToFile();
-        }
-        catch (IOException e) {
+            saveToFile();
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void loadFromFile() throws IOException{
-        if(!file.exists()){
+    public static void loadFromFile() throws IOException {
+        if (!file.exists()) {
             throw new IOException("Файл с задачами не найден:(");
         }
         Scanner s = new Scanner(file);
 
         while (s.hasNext()) {
             String[] parseTask = s.nextLine().split(";");
-            Task task = new Task();
-            task.id = Integer.parseInt(parseTask[0]);
-            task.status = Boolean.parseBoolean(parseTask[1]);
-            task.description = parseTask[2];
-            task.createdAt = LocalDateTime.parse(parseTask[3],formatter);
+            Integer id = Integer.parseInt(parseTask[0]);
+            boolean status = Boolean.parseBoolean(parseTask[1]);
+            String description = parseTask[2];
+            LocalDateTime createdAt = LocalDateTime.parse(parseTask[3], formatter);
+            Task task = new Task(id, description, status, createdAt);
             list.add(task);
         }
         s.close();
     }
 
-    public static void saveToFile(){
+    public static void saveToFile() {
         try (PrintWriter writer = new PrintWriter(new FileWriter(file))) {
             for (Task task : list) {
-                writer.println(task.id + ";" + task.status + ";" + task.description + ";" + task.createdAt.format(formatter));
+                writer.println(task.getId() + ";" + task.getStatus() + ";" + task.getDescription() + ";" + task.getTime().format(formatter));
             }
         } catch (IOException e) {
             System.out.println("Ошибка при записи в файл: " + e.getMessage());
         }
     }
 
-    public static void addTask(String description){
-        if(description.contains(";")){
+    public static void addTask(String description) {
+        if (description.contains(";")) {
             System.out.println("описание задачи не может содержать в себе символ ;");
-        }
-        else{
-        Task task = new Task();
-        task.description = description;
-        task.status = false;
-        task.createdAt = java.time.LocalDateTime.now();
-        list.add(task);
-        task.id = list.indexOf(task) + 1;
+        } else {
+            Task task = new Task(list.size() + 1, description, false, java.time.LocalDateTime.now());
+            list.add(task);
         }
     }
 
 
-    public static void listTasks(){
-        if(list.isEmpty()){
+    public static void listTasks() {
+        if (list.isEmpty()) {
             System.out.println("В списке задач пусто :(");
-        }
-        else{
-        for (Task task: list) {
-            printTask(task);
+        } else {
+            for (Task task : list) {
+                printTask(task);
             }
         }
     }
 
-    public static void markTaskAsDone(int id){
-        for(Task task: list){
-            if(task.id == id){
-                if(task.status){
+    public static void markTaskAsDone(int id) {
+        for (Task task : list) {
+            if (task.getId() == id) {
+                if (task.getStatus()) {
                     System.out.println("задача которую вы хотите отметить уже выполнена");
+                } else {
+                    task.setStatus(true);
                 }
-                else{
-                    task.status = true;
-                }
-                System.out.println("Поздравляю с выполнением задачи: " + task.description + "!");
+                System.out.println("Поздравляю с выполнением задачи: " + task.getDescription() + "!");
                 return;
             }
         }
         System.out.println("Кажется вы ввели некорректный номер задачи :(");
     }
 
-    public static void deleteCompletedTasks(){
-        list.removeIf(task -> task.status);
-        for( Task task: list){
-            task.id = list.indexOf(task)+1;
+    public static void deleteCompletedTasks() {
+        list.removeIf(Task::getStatus);
+        for (Task task : list) {
+            task.setId(list.indexOf(task) + 1);
         }
         System.out.println("Выполненные задачи успешно удалены! ");
     }
 
-    public static void  searchTasks(String keyword){
+    public static void searchTasks(String keyword) {
         System.out.println("Все задачи, содержащие в себе " + keyword);
-        for( Task task: list){
-            if(task.description.contains(keyword)){
+        for (Task task : list) {
+            if (task.getDescription().contains(keyword)) {
                 printTask(task);
             }
         }
     }
 
-    public static void printTask(Task task){
-        String  status;
-        if (task.status){
+    public static void printTask(Task task) {
+        String status;
+        if (task.getStatus()) {
             status = "Выполненая задача:";
-        }
-        else{
+        } else {
             status = "Активная задача:";
         }
 
-        String formatedTime = task.createdAt.format(formatter);
-        System.out.println(task.id + " " + status + " " + task.description + " " + formatedTime);
+        String formatedTime = task.getTime().format(formatter);
+        System.out.println(task.getId() + " " + status + " " + task.getDescription() + " " + formatedTime);
         System.out.println("_______________________________________________________________");
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,48 @@
 import java.time.LocalDateTime;
 
 public class Task {
-    Integer id;
-    String description;
-    boolean status;
-    LocalDateTime createdAt;
+    private Integer id;
+    private String description;
+    private boolean status;
+    private LocalDateTime createdAt;
+
+    public Task(Integer id, String description, boolean status, LocalDateTime createdAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTime() {
+        return this.createdAt;
+    }
+
+    public void setTime(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
